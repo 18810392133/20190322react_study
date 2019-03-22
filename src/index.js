cdb4f68d6a0acf59b7f056fa345b7225b7837237
2541747035@qq.com <wjl984296155@#>
2019-03-22 21:55:22 +0800

    设置<clock/>组件，实现计时功能

--- src/index.js
+++ src/index.js
@@ -4,41 +4,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-class Clock extends React.Component {
-    constructor(props) {
+class Clock extends React.Component{
+    constructor(props){
         super(props);
-        this.state = {date: new Date()};
+        this.state={date:new Date()};
     }
-
-    /*设置生命周期方法*/
-    /*会在组件被渲染到DOM之后运行*/
     componentDidMount() {
-        /*箭头函数：左边需要写的是函数的参数，右边是进行的操作和返回的值*/
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-    componentWillUnmount() {
-        clearInterval(this.timerID)
+        this.timerID = setInterval(()=> this.tick(),1000);
     }
 
-    /**/
+
     tick(){
-        this.setState({date:new Date()});
+        this.setState({date:new Date()})
     }
+
 
     render() {
         return (
-            <h2>the time is {this.state.date.toLocaleTimeString()}</h2>
+            <h2>this time is {this.state.date.toLocaleTimeString()}</h2>
         );
     }
-
-
 }
 
-ReactDOM.render(<Clock/>,document.getElementById('root'))
 
+ReactDOM.render(<Clock />,document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
